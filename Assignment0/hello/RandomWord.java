9b0d2813ad3d5c2a4ec629fcc237587c624764c3
polachandu <polachandrahas@gmail.com>
2023-09-04 23:31:40 -0700

    Assignment0

---
+++ Assignment0/hello/RandomWord.java
@@ -0,0 +1,17 @@
+import edu.princeton.cs.algs4.StdIn;
+
+public class RandomWord {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    private static String randomWord(){
+        int count = 0;
+        while(!StdIn.isEmpty()){
+            count++;
+        }
+        
+    }
+}
